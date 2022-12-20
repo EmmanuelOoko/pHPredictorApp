d3f6cb5bbdea7de4a8dd0429af5d587624a3926e
EmmanuelOoko <38856562+EmmanuelOoko@users.noreply.github.com>
2022-12-20 14:32:38 +0300

    update predictor_app.py

--- predictor_app.py
+++ predictor_app.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pandas as pd
 
 #Loading up the Classification model we created
-model = xgb.XGBClassifier()
-model.load_model('ph_model.json')
+#model = xgb.XGBClassifier()
+#model.load_model('ph_model.json')
 
 #Caching the model for faster loading
 @st.cache
