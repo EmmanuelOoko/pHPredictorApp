47e0a2d9152ea0b248203b9e3b5dcf668f8ff188
EmmanuelOoko <38856562+EmmanuelOoko@users.noreply.github.com>
2022-12-20 15:15:53 +0300

    Update predictor_app.py

--- predictor_app.py
+++ predictor_app.py
@@ -23,59 +23,29 @@ from  PIL import Image, ImageEnhance
 #Caching the model for faster loading
 @st.cache
 
-def predict(carat, cut, color, clarity, depth, table, x, y, z):
-    #Predicting the price of the carat
-    if cut == 'Fair':
-        cut = 0
-    elif cut == 'Good':
-        cut = 1
-    elif cut == 'Very Good':
-        cut = 2
-    elif cut == 'Premium':
-        cut = 3
-    elif cut == 'Ideal':
-        cut = 4
-
-    if color == 'J':
-        color = 0
-    elif color == 'I':
-        color = 1
-    elif color == 'H':
-        color = 2
-    elif color == 'G':
-        color = 3
-    elif color == 'F':
-        color = 4
-    elif color == 'E':
-        color = 5
-    elif color == 'D':
-        color = 6
-    
-    if clarity == 'I1':
-        clarity = 0
-    elif clarity == 'SI2':
-        clarity = 1
-    elif clarity == 'SI1':
-        clarity = 2
-    elif clarity == 'VS2':
-        clarity = 3
-    elif clarity == 'VS1':
-        clarity = 4
-    elif clarity == 'VVS2':
-        clarity = 5
-    elif clarity == 'VVS1':
-        clarity = 6
-    elif clarity == 'IF':
-        clarity = 7
-    
-
-    prediction = model.predict(pd.DataFrame([[carat, cut, color, clarity, depth, table, x, y, z]], columns=['carat', 'cut', 'color', 'clarity', 'depth', 'table', 'x', 'y', 'z']))
-    return prediction
-
 st.title('pH Level Predictor')
 st.image('ph_kit.jpg')
 st.header('Enter the Color Image:')
 
+
+#Create two columns with different width
+col1, col2 = st.columns( [0.8, 0.2])
+with col1:               # To display the header text using css style
+    st.markdown(""" <style> .font {
+    font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+    </style> """, unsafe_allow_html=True)
+    st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
+    
+with col2:               # To display brand logo
+    st.image(image,  width=150)
+  
+  #Add a header and expander in side bar
+st.sidebar.markdown('<p class="font">My First Photo Converter App</p>', unsafe_allow_html=True)
+with st.sidebar.expander("About the App"):
+     st.write("""
+        Use this simple app to convert your favorite photo to a pencil sketch, a grayscale image or an image with blurring effect.  \n  \nThis app was created by Sharone Li as a side project to learn Streamlit and computer vision. Hope you enjoy!
+     """)
+  
 uploaded_file = st.file_uploader("Choose a file", type=['jpg','png','jpeg'])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
@@ -85,6 +55,9 @@ if uploaded_file is not None:
         st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
         st.image(image,width=300) 
 
+   with col2:
+        st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
+     
     # To convert to a string based IO:
     #stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
     #st.write(stringio)
@@ -96,25 +69,3 @@ if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     #dataframe = pd.read_csv(uploaded_file)
     #st.write(dataframe)
-
-carat = st.number_input('Carat Weight:', min_value=0.1, max_value=10.0, value=1.0)
-
-cut = st.selectbox('Cut Rating:', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
-
-color = st.selectbox('Color Rating:', ['J', 'I', 'H', 'G', 'F', 'E', 'D'])
-
-clarity = st.selectbox('Clarity Rating:', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
-
-depth = st.number_input('Diamond Depth Percentage:', min_value=0.1, max_value=100.0, value=1.0)
-
-table = st.number_input('Diamond Table Percentage:', min_value=0.1, max_value=100.0, value=1.0)
-
-x = st.number_input('Diamond Length (X) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-
-y = st.number_input('Diamond Width (Y) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-
-z = st.number_input('Diamond Height (Z) in mm:', min_value=0.1, max_value=100.0, value=1.0)
-
-if st.button('Predict Price'):
-    price = predict(carat, cut, color, clarity, depth, table, x, y, z)
-    st.success(f'The predicted price of the diamond is ${price[0]:.2f} USD')
